2aa2148f80b901477cb141b7180fb3e27cce73e1
Mehdiakha <akhabbarmehdi@gmail.com>
2025-08-20 16:36:01 +0100

    projects readme

--- day01/webserver/main.py
+++ day01/webserver/main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from webserver!")
+import socket
 
+def main():
+    
+    HOST = "0.0.0.0"
+    PORT = 8080
+
+    socket__ervice = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
 
 if __name__ == "__main__":
     main()
